2adeffd8105e9c3a6c2d0a55b0475d7530815031
Goodhao <goodhao2012@gmail.com>
2021-11-10 20:36:50 +0800

    fix some bugs

--- automaton.cpp
+++ automaton.cpp
@@ -121,6 +121,7 @@ automaton automaton::turn_to_NFA() {
 		set<string> c = eps_closure(x);
 		for (auto y : c) {
 			if (y != x) {
+				if (_final_state.count(y) != 0) res._final_state.insert(x);
 				for (auto chr : _sigma) if (chr != "eps") {
 					if (_to.count(make_pair(y, chr)) != 0) {
 						for (auto z : _to[make_pair(y, chr)]) {
